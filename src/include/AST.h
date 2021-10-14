1bb13b9bddbb6063207fb66829fc46b37b94a0af
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-14 17:15:13 +0900

    the ast stuff

--- src/include/AST.h
+++ src/include/AST.h
@@ -6,14 +6,33 @@
 typedef struct AST_STRUCT
 {
     enum {
-        AST_PRINT,
+        AST_VARIABLE_DEFINITION,
+        AST_FUNCTION_DEFINITION,
+        AST_VARIABLE,
+        AST_FUNCTION_CALL,
         AST_STRING,
         AST_COMPOUND,
         AST_NOOP,
     } type;
 
-    // AST_PRINT
-    struct AST_STRUCT* print_value;
+    struct SCOPE_STRUCT* scope;
+
+    // AST_VARIABLE_DEFINITION
+    char* variable_definition_variable_name;
+    struct AST_STRUCT* variable_definition_value;
+
+    // AST_FUNCTION_DEFINITION
+    struct AST_STRUCT* function_definition_body;
+    char* function_definition_name;
+
+    // AST_VARIABLE
+    char* variable_name;
+
+    // AST_FUNCTION_CALL
+    char* function_call_name;
+    char* function_call_args_name;
+    struct AST_STRUCT** function_call_arguments;
+    size_t function_call_arguments_size;
 
     // AST_STRING
     char* string_value;
