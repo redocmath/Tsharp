d3efd12755e02db7e5ee072cc7ff3786a4a5c3e5
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-14 17:22:32 +0900

    added scope to parser

--- src/include/parser.h
+++ src/include/parser.h
@@ -2,6 +2,7 @@
 #define PARSER_H
 #include "AST.h"
 #include "lex.h"
+#include "scope.h"
 
 typedef struct PARSER_STRUCT
 {
@@ -10,24 +11,30 @@ typedef struct PARSER_STRUCT
     token_T* current_token;
 
     token_T* prev_token;
+
+    scope_T* scope;
 } parser_T;
 
 parser_T* init_parser(lexer_T* lexer);
 
 void parser_eat(parser_T* parser, int token_type);
 
-AST_T* parser_parse(parser_T* parser);
+AST_T* parser_parse(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_statement(parser_T* parser);
+AST_T* parser_parse_statement(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_statements(parser_T* parser);
+AST_T* parser_parse_statements(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_expr(parser_T* parser);
+AST_T* parser_parse_expr(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_string(parser_T* parser);
+AST_T* parser_parse_variable_definition(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_id(parser_T* parser);
+AST_T* parser_parse_function_definition(parser_T* parser, scope_T* scope);
 
-AST_T* parser_parse_print(parser_T* parser);
+AST_T* parser_parse_string(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_id(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_function_call(parser_T* parser, scope_T* scope);
 
 #endif
