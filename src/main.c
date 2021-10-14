6c82d5c0a994c97b6c2fcbfe5d76a9dc92702b67
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-14 17:19:28 +0900

    main file...

--- src/main.c
+++ src/main.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "include/lex.h"
 #include "include/parser.h"
+#include "include/visitor.h"
+#include "include/io.h"
+#include "include/scope.h"
 
 int main(int argc, char* argv[]) {
 
     lexer_T* lexer = init_lexer(
-        "print :: \"Hello World!\";\n"
+        get_file_contents(argv[1])
     );
 
     parser_T* parser = init_parser(lexer);
-    AST_T* root = parser_parse(parser);
 
-    printf("%d\n", root->type);
-    printf("%zu\n", root->compound_size);
+    AST_T* root = parser_parse(parser, parser->scope);
+
+    visitor_T* visitor = init_visitor();
+
+    visitor_visit(visitor, root);
+
+    char* name = "main";
+    AST_T* fdef = scope_find_func(parser->scope, name);
+
+    system("gcc -o main comp.c");
 
     return 0;
 }
