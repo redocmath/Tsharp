b3fcc5275ffe0e5fb1a6cc4c28660db6f848b213
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-15 15:34:33 +0900

    added parser for inside function body

--- src/include/parser.h
+++ src/include/parser.h
@@ -23,17 +23,27 @@ AST_T* parser_parse(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_statement(parser_T* parser, scope_T* scope);
 
+AST_T* parser_parse_statement_func_body(parser_T* parser, scope_T* scope);
+
 AST_T* parser_parse_statements(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_statements_func_body(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_expr(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_variable_definition(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_variable(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_variable_outside_func(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_function_definition(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_string(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_id(parser_T* parser, scope_T* scope);
+
+AST_T* parser_parse_id_func_body(parser_T* parser, scope_T* scope);
 
 AST_T* parser_parse_function_call(parser_T* parser, scope_T* scope);
 
