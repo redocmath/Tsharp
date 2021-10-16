6d3b74560de331bad9e27ee1f5f8441fbf35548d
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-16 19:18:01 +0900

    compile to assembly and write to file

--- src/asm.c
+++ src/asm.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* ↓ change this later */
+char mainfile[1000];
+char datafile[1000];
+char* contents;
+
 static AST_T* builtin_function_print(visitor_T* visitor, AST_T** args, int args_size, char* name)
 {
     for (int i = 0; i < args_size; i++)
@@ -12,89 +17,33 @@ static AST_T* builtin_function_print(visitor_T* visitor, AST_T** args, int args_
         AST_T* visited_ast = visitor_visit(visitor, args[i]);
         if (visited_ast->type == AST_STRING)
         {
-            char* buffer = 0;
-            buffer = visitor_read_current_comp_file();
+            contents = "\nmov rax, 0x2000004\n"
+                       "mov rdi, 1\n"
+                       "mov rsi, ";
+            strcat(mainfile, contents);
+            strcat(mainfile, name);
             
-            FILE *fp;
-            fp = fopen("comp.c", "w");
+            contents = "\nmov rdx, ";
+            strcat(mainfile, contents);
+            strcat(mainfile, name);
 
-            if (fp)
-            {
-                fprintf(fp, "%s", buffer);
-                fprintf(fp, "%s", "\nprintf(\"%s\",");
-                fprintf(fp, "%s", name);
-                fprintf(fp, "%s", ");");
-                fclose(fp);
-            }
+            contents = "len\n"
+                       "\nsyscall\n";
+            strcat(mainfile, contents);
         }
     }
     return init_ast(AST_NOOP);
-}
-
-static AST_T* builtin_function_print_nonval(visitor_T* visitor, AST_T** args, int args_size)
-{
-    for (int i = 0; i < args_size; i++)
-    {
-        AST_T* visited_ast = visitor_visit(visitor, args[i]);
-        if (visited_ast->type == AST_STRING)
-        {
-            char* buffer = 0;
-            buffer = visitor_read_current_comp_file();
-            
-            FILE *fp;
-            fp = fopen("comp.c", "w");
-
-            if (fp)
-            {
-                fprintf(fp, "%s", buffer);
-                fprintf(fp, "%s", "\nprintf(\"%s\",");
-                fprintf(fp, "%s", "\"");
-                fprintf(fp, "%s", visited_ast->string_value);
-                fprintf(fp, "%s", "\"");
-                fprintf(fp, "%s", ");");
-                fclose(fp);
-            }
-        }
-    }
-    return init_ast(AST_NOOP);
-}
-
-char* visitor_read_current_comp_file()
-{
-    char* buffer = 0;
-    long length;
-
-    FILE*f = fopen("comp.c", "rb");
-
-    if (f)
-    {
-        fseek(f, 0, SEEK_END);
-        length = ftell(f);
-        fseek(f, 0, SEEK_SET);
-
-        buffer = calloc(length, length);
-
-        if (buffer)
-            fread(buffer, 1, length, f);
-
-        fclose(f);
-
-        return buffer;
-    }
-
-    printf("File Does not exist!\n");
-
-    exit(1);
 }
 
 visitor_T* init_visitor()
 {
-    FILE *fp;
-    fp = fopen("comp.c", "w");
-    fprintf(fp, "%s", "#include <stdio.h>\n");
-    fprintf(fp, "%s", "#include <stdlib.h>\n");
-    fprintf(fp, "%s", "#include <stdio.h>\n\n");
-    fclose(fp);
+    // Main Section
+    contents = "; Assembly Main Section\n";
+    strcat(mainfile, contents);
+
+    // Data Section
+    contents = "section .data\n";
+    strcat(datafile, contents);
 
     visitor_T* visitor = calloc(1, sizeof(struct VISITOR_STRUCT));
 
@@ -125,32 +74,7 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
 {
     if (strcmp(node->function_call_name, "print") == 0)
     {
-        if (node->function_call_args_name == (void*) 0) {
-            return builtin_function_print_nonval(visitor, node->function_call_arguments, node->function_call_arguments_size);
-        }
         return builtin_function_print(visitor, node->function_call_arguments, node->function_call_arguments_size, node->function_call_args_name);
-    }
-
-    scope_find_func(node->scope, node->function_call_name);
-
-    if (1) {
-        char* buffer = 0;
-        buffer = visitor_read_current_comp_file();
-        
-        FILE *fp;
-        fp = fopen("comp.c", "w");
-
-        if (fp)
-        {
-            fprintf(fp, "%s", buffer);
-
-            fprintf(fp, "%s", "\n");
-            fprintf(fp, "%s", node->function_call_name);
-            fprintf(fp, "%s", "(");
-            fprintf(fp, "%s", ");");
-            
-            fclose(fp);
-        }
     }
 
     return node;
@@ -162,29 +86,34 @@ AST_T* visitor_visit_variable_definition(visitor_T* visitor, AST_T* node)
         node->scope,
         node
     );
-    
-    char* buffer = 0;
-    buffer = visitor_read_current_comp_file();
 
     AST_T* visited_ast = visitor_visit(visitor, node->variable_definition_value);
-    
-    FILE *fp;
-    fp = fopen("comp.c", "w");
 
-    if (fp)
-    {
-        fprintf(fp, "%s", buffer);
-        if (visited_ast->type == AST_STRING) {
-            fprintf(fp, "%s", "\nchar* ");
-            fprintf(fp, "%s", node->variable_definition_variable_name);
-            fprintf(fp, "%s", " = ");
-            fprintf(fp, "%s", "\"");
-            fprintf(fp, "%s", visited_ast->string_value);
-            fprintf(fp, "%s", "\";");
-        }
-        
-        fclose(fp);
-    }
+    contents = node->variable_definition_variable_name;
+    strcat(datafile, contents);
+
+    contents = " db "
+               "\"";
+    strcat(datafile, contents);
+
+    contents = visited_ast->string_value;
+    strcat(datafile, contents);
+
+    contents = "\", 0x0a\n";
+    strcat(datafile, contents);
+
+    contents = node->variable_definition_variable_name;
+    strcat(datafile, contents);
+
+    contents = "len"
+               ": equ $ - ";
+    strcat(datafile, contents);
+    
+    contents = node->variable_definition_variable_name;
+    strcat(datafile, contents);
+
+    contents = "\n";
+    strcat(datafile, contents);
 
     return node;
 }
@@ -196,39 +125,18 @@ AST_T* visitor_visit_function_definition(visitor_T* visitor, AST_T* node)
         node
     );
 
-    if (1) {
-        char* buffer = 0;
-        buffer = visitor_read_current_comp_file();
-        
-        FILE *fp;
-        fp = fopen("comp.c", "w");
+    contents ="GLOBAL _main\n\n"
+               "section .text\n"
+               "global start\n\n"
+               "_main:\n";
+    strcat(mainfile, contents);
 
-        if (fp)
-        {
-            fprintf(fp, "%s", buffer);
+    visitor_visit(visitor, node->function_definition_body);
 
-            fprintf(fp, "%s", "\nint ");
-            fprintf(fp, "%s", node->function_definition_name);
-            fprintf(fp, "%s", "() {");
-            fclose(fp);
-        }
-
-        visitor_visit(visitor, node->function_definition_body);
-
-        buffer = 0;
-        buffer = visitor_read_current_comp_file();
-
-        fp = fopen("comp.c", "w");
-
-        if (fp)
-        {
-            fprintf(fp, "%s", buffer);
-
-            fprintf(fp, "%s", "\nreturn 0;");
-            fprintf(fp, "%s", "\n};\n\n");
-            fclose(fp);
-        }
-    }
+    contents = "\nmov rax, 0x2000001\n"
+               "mov rdi, 0\n"
+               "syscall\n";
+    strcat(mainfile, contents);
 
     return node;
 }
@@ -258,6 +166,20 @@ AST_T* visitor_visit_compound(visitor_T* visitor, AST_T* node)
     for (int i = 0; i < node->compound_size; i++)
     {
         visitor_visit(visitor, node->compound_value[i]);
+    }
+
+    /* ↓ change this later */
+    char finalcontents[1000];
+
+    strcpy(finalcontents, mainfile);
+    strcat(finalcontents, datafile);
+
+    FILE *fp;
+    fp = fopen("main.asm", "w");
+    if (fp)
+    {
+        fprintf(fp, "%s", finalcontents);
+        fclose(fp);
     }
 
     return init_ast(AST_NOOP);    
