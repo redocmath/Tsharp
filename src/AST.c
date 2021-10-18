7cb90bfe8f9f20336fa2e6d4607aec5d6a8d844b
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-18 20:19:49 +0900

    added ast function call args

--- src/AST.c
+++ src/AST.c
@@ -16,18 +16,13 @@ AST_T* init_ast(int type)
     ast->function_definition_body = (void*) 0;
     ast->function_definition_name = (void*) 0;
 
-    // AST_PRINT
-    ast->print_args = (void*) 0;
-    ast->print_size = 0;
-
     // AST_VARIABLE
     ast->variable_name = (void*) 0;
 
     // AST_FUNCTION_CALL
     ast->function_call_name = (void*) 0;
-    ast->function_call_arguments = (void*) 0;
-    ast->function_call_args_name = (void*) 0;
-    ast->function_call_arguments_size = 0;
+    ast->args = (void*) 0;
+    ast->args_size = 0;
 
     // AST_STRING
     ast->string_value = (void*) 0;
