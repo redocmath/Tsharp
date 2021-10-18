1390e2b1edec8f24ed3bd0f250f1f4fa289edf1c
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-18 20:19:10 +0900

    added ast function call args

--- src/include/AST.h
+++ src/include/AST.h
@@ -8,7 +8,6 @@ typedef struct AST_STRUCT
     enum {
         AST_VARIABLE_DEFINITION,
         AST_FUNCTION_DEFINITION,
-        AST_PRINT,
         AST_VARIABLE,
         AST_FUNCTION_CALL,
         AST_STRING,
@@ -25,19 +24,14 @@ typedef struct AST_STRUCT
     // AST_FUNCTION_DEFINITION
     struct AST_STRUCT* function_definition_body;
     char* function_definition_name;
-
-    // AST_PRINT
-    struct AST_STRUCT** print_args;
-    size_t print_size;
     
     // AST_VARIABLE
     char* variable_name;
 
     // AST_FUNCTION_CALL
     char* function_call_name;
-    char* function_call_args_name;
-    struct AST_STRUCT** function_call_arguments;
-    size_t function_call_arguments_size;
+    struct AST_STRUCT** args;
+    size_t args_size;
 
     // AST_STRING
     char* string_value;
