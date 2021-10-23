a104602768f0baa38ad73c8c78a61fb4c8911d91
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-23 21:11:59 +0900

    added tokens

--- src/include/token.h
+++ src/include/token.h
@@ -5,18 +5,22 @@ typedef struct TOKEN_STRUCT
 {
     enum
     {
-        TOKEN_STRING, // "abc"
-        TOKEN_ID, // abc
-        TOKEN_SEMI, // ;
-        TOKEN_LPAREN, // (
-        TOKEN_RPAREN, // )
-        TOKEN_LBRACE, // {
-        TOKEN_RBRACE, // }
-        TOKEN_EQUAL, // =
-        TOKEN_COMMA, // ,
-        TOKEN_DOT, // .
-        TOKEN_INT, // 123
-        TOKEN_EOF // End Of File
+        /* 00 */TOKEN_STRING, // "abc"
+        /* 01 */TOKEN_ID, // abc
+        /* 02 */TOKEN_SEMI, // ;
+        /* 03 */TOKEN_LPAREN, // (
+        /* 04 */TOKEN_RPAREN, // )
+        /* 05 */TOKEN_LBRACE, // {
+        /* 06 */TOKEN_RBRACE, // }
+        /* 07 */TOKEN_EQUAL, // =
+        /* 08 */TOKEN_COMMA, // ,
+        /* 09 */TOKEN_DOT, // .
+        /* 10 */TOKEN_GREATER_THAN,
+        /* 11 */TOKEN_LESS_THAN,
+        /* 12 */TOKEN_EQUALS,
+        /* 13 */TOKEN_NOT_EQUALS,
+        /* 14 */TOKEN_INT, // 123
+        /* 15 */TOKEN_EOF // End Of File
     } type;
 
     char* value;
