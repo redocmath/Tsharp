d6efdc6cb307a13d10fb72488ec0fbf2ead30b62
Ibukiyoshidaa <ibuki042004@gmail.com>
2021-10-26 04:29:44 +0900

    edited visitor visit fucntion call

--- src/visitor.c
+++ src/visitor.c
@@ -73,6 +73,23 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node)
         exit(1);
     }
 
+    for (int i = 0; i < (int) node->args_size; i++)
+    {
+        AST_T* ast_var = (AST_T*) fdef->function_definition_args[i];
+
+        AST_T* ast_value = (AST_T*) node->args[i];
+
+        AST_T* ast = init_ast(AST_VARIABLE_DEFINITION);
+
+        ast->variable_definition_value = ast_value;
+
+        ast->variable_definition_variable_name = calloc(strlen(ast_var->variable_name) + 1, sizeof(char));
+        ast->variable_definition_func_name = calloc(strlen(node->function_call_name) + 1, sizeof(char));
+        strcpy(ast->variable_definition_variable_name, ast_var->variable_name);
+
+        scope_add_variable_definition(fdef->function_definition_body->scope, ast);
+    }
+
     visitor_visit(visitor, fdef->function_definition_body);
 
     return node;
